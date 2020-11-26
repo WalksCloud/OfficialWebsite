78c2d2cd1229a0780c91489e90a62a41cf63f1f9
腹黒い茶 <haraguroicha@tipsy.coffee>
2020-11-26 22:03:58 +0800

    update about us description content

--- lang/en-US.js
+++ lang/en-US.js
@@ -2,5 +2,6 @@ export default {
   companyName: 'Walks Cloud',
   companyNameSlogan: 'Makes your Cloud Works Great',
   aboutUS: 'About US',
-  aboutUSDesc: 'About US Desc',
+  aboutUSDesc:
+    "We're creating a enterprise ready of Cloud application, the goal is provide users with everything that is needed to build rich and engaging management applications using ours products.",
 }
