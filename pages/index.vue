1c1c991e8fa114f2c0567cac51fc2e9e4485e7f4
腹黒い茶 <haraguroicha@walks.cloud>
2022-02-28 01:33:15 +0800

    update contact us with comment out the form

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     <c-03-features></c-03-features>
     <c-04-stats></c-04-stats>
     <!-- <c-05-blog></c-05-blog> -->
-    <!-- <c-06-contact-us></c-06-contact-us> -->
+    <c-06-contact-us></c-06-contact-us>
   </div>
 </template>
 
