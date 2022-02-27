9a6a03c6781a5043187274ef17935c4448de38c0
腹黒い茶 <haraguroicha@walks.cloud>
2022-02-28 00:35:17 +0800

    update packages and lint and config

--- nuxt.config.js
+++ nuxt.config.js
@@ -90,6 +90,8 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
