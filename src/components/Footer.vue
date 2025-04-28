f1238b77fd030252161fd5f8092e158d4ce3e8d1
腹黒い茶 <haraguroicha@walks.cloud>
2025-04-28 15:25:08 +0800

    add build hash and build time to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,7 @@
+<script setup>
+  const env = import.meta.env;
+</script>
+
 <template>
 	<footer class="text-center px-3 pb-3 lg:py-6 lg:px-0 text-sm lg:text-base">
     <p>
@@ -8,6 +12,8 @@
     <a :href="$t('footer.facebook_url')" target="_black" class="inline-block my-2 me-2"><font-awesome-icon :icon="['fab', 'square-facebook']" class="text-2xl text-gray-600" /></a>
     <a :href="$t('footer.linkedin_url')" target="_black" class="inline-block my-2"><font-awesome-icon :icon="['fab', 'linkedin']" class="text-2xl text-gray-600" /></a>
     <p class="text-sm/1">
+      <a href="https://github.com/WalksCloud/OfficialWebsite">GitHub Repo</a>
+			(<a :href="`https://github.com/WalksCloud/OfficialWebsite/commit/` + env.buildHash">#{{env.buildHash}}</a> {{env.buildTime}}) | 
       <a :href="$t('footer.term-of-use_url')" target="_blank">{{ $t('footer.term-of-use') }}</a> | 
       <a :href="$t('footer.privacy-policy_url')" target="_blank">{{ $t('footer.privacy-policy') }}</a>
     </p>
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { DateTime } from "luxon";
+import git from 'git-rev-sync'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: {
+      'import.meta.env.buildTime': JSON.stringify(DateTime.now().toFormat('yyyy-MM-dd HH:mm:ss ZZ')),
+      'import.meta.env.buildHash': JSON.stringify(git.short(".") + (git.isDirty() ? '-dirty' : '')),
+      'import.meta.env.__APP_ENV__': JSON.stringify(env.APP_ENV),
+    },
+    plugins: [
+      vue(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
     }
   }
 })
