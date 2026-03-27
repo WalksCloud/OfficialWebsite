1d5e7bf1afcbc1382c137d6ad15fd02a53bcc3fa
腹黒い茶 <haraguroicha@walks.cloud>
2026-03-28 01:26:45 +0800

    fix locale url path

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faSquareFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import routes, { scrollBehavior } from './router'
-import { getSiteConfig } from './utils/pageConfig'
+import { getSiteConfig, buildPrefixedPath, buildNonPrefixedPath } from './utils/pageConfig'
 
 import en from './locales/en-US.yaml'
 import tw from './locales/zh-TW.yaml'
@@ -43,6 +43,11 @@ export const createApp = ViteSSG(
       }
     }
 
+    const normalizePath = (p) => {
+      const v = (p || '').replace(/\/+$/, '')
+      return v || '/'
+    }
+
     // During build, store locale in initialState for hydration under non-prefix paths
     router.beforeEach((to, from, next) => {
       const storedLocale = isClient ? localStorage.getItem('locale') : null
@@ -53,6 +58,33 @@ export const createApp = ViteSSG(
         initialLocale ||
         site.defaultLocale ||
         'zh-tw'
+      // Align URL with stored locale
+      if (isClient && storedLocale) {
+        const pageKey = to.meta.pageKey || 'home'
+        const currentPath = normalizePath(to.path)
+
+        if (storedLocale === site.defaultLocale) {
+          // Default locale prefers non-prefixed URL
+          const nonPrefixedPath = normalizePath(buildNonPrefixedPath(pageKey, storedLocale))
+          if (to.meta.prefixed === true && currentPath !== nonPrefixedPath) {
+            to.meta.locale = storedLocale
+            if (initialState && !initialState.locale) initialState.locale = storedLocale
+            setLocale(storedLocale)
+            return next(nonPrefixedPath)
+          }
+        } else {
+          // Non-default locale prefers prefixed URL
+          if (to.meta.prefixed !== true) {
+            const prefixedPath = normalizePath(buildPrefixedPath(pageKey, storedLocale))
+            if (prefixedPath && currentPath !== prefixedPath) {
+              to.meta.locale = storedLocale
+              if (initialState && !initialState.locale) initialState.locale = storedLocale
+              setLocale(storedLocale)
+              return next(prefixedPath)
+            }
+          }
+        }
+      }
       to.meta.locale = targetLocale
       if (initialState && !initialState.locale) {
         initialState.locale = targetLocale
